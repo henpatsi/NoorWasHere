cba09dc808ba043c4ebc3fab1aa08fbb3640619d
Henri Pätsi <patsihenri@gmail.com>
2024-09-12 12:16:25 +0300

    refactor picture movement and entering/exiting

--- levels/test_level/level_3/picture_handler_3.gd
+++ levels/test_level/level_3/picture_handler_3.gd
@@ -5,19 +5,14 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if currentPicture:
+		currentPicture.active_picture = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	currentPicture.check_player_position()
+	pass
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("swap_places"):
-		if !currentPicture:
-			print("No picture to swap with")
-			return
-		currentPicture.enter_picture()
-
 	if event.is_action_pressed("inspect_picture"):
 		if !currentPicture:
 			print("No picture to inspect")
