5a6dadfe254bdff5aabf28ebaac7ff9c410bfc11
Henri Pätsi <patsihenri@gmail.com>
2024-09-13 16:02:05 +0300

    redoin trigger arae audio for dialogue subs

--- levels/test_level/trigger_area.gd
+++ levels/test_level/trigger_area.gd
@@ -1,21 +1,27 @@
 extends Area3D
 
+class SubtitledAudioClip:
+	var audioClip: AudioStream
+	var subtitle: String
+
 @export_category("Settings")
 @export var one_shot: bool = false
-var triggered: bool = false
-var wait_triggered: bool = false
 
 @export_category("Audio")
 @export var audio_stream_player: AudioStreamPlayer3D
-@export var audio_stream: AudioStream
+@export var subtitle_label: Label
+@export var audio_clips: Array[SubtitledAudioClip]
 @export var override_playing: bool = false
+var audio_index = -1
 
-@export_category("Activate")
-@export var activate_wait_for_audio: bool = false
-@export var nodes_to_activate: Array[Node]
-@export var nodes_to_deactivate: Array[Node]
-@export var nodes_to_show: Array[Node]
-@export var nodes_to_hide: Array[Node]
+# @export_category("Activate")
+# @export var activate_wait_for_audio: bool = false
+# @export var nodes_to_activate: Array[Node]
+# @export var nodes_to_deactivate: Array[Node]
+# @export var nodes_to_show: Array[Node]
+# @export var nodes_to_hide: Array[Node]
+
+var active: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,43 +30,48 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if not active:
+		return
+	
+	if audio_stream_player and audio_clips.size() > 0:
+		handle_audio_clip()
+	
+	if not active and one_shot:
+		queue_free()
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if triggered and one_shot:
+	if active:
 		return
 
 	if not body.is_in_group("Player"):
 		return
 	
 	print("Area triggered")
-	wait_triggered = false
-
-	audio_trigger()
-	activate_trigger()
-
-	if one_shot:
-		queue_free()
+	active = true
+	audio_index = -1
 
 
-func audio_trigger() -> void:
-	# Play audio if set and conditions met
-	if audio_stream_player and audio_stream:
-		if override_playing or audio_stream_player.playing == false:
-			audio_stream_player.stream = audio_stream
-			audio_stream_player.play()
-			
+func handle_audio_clip() -> void:
+	if audio_stream_player.playing:
+		return
 
-func activate_trigger() -> void:
-	if activate_wait_for_audio and audio_stream_player and audio_stream:
-		await get_tree().create_timer(audio_stream.get_length()).timeout
+	audio_index += 1
+	if audio_index >= audio_clips.size():
+		active = false
+		return
+	
+	audio_stream_player.stream = audio_clips[audio_index].audioClip
+	audio_stream_player.play()
 
-	for node in nodes_to_activate:
-		node.set_process(true)
-	for node in nodes_to_deactivate:
-		node.set_process(false)
-	for node in nodes_to_show:
-		node.show()
-	for node in nodes_to_hide:
-		node.hide()
+	# TODO subtitles
+
+# func activate_trigger() -> void:
+# 	for node in nodes_to_activate:
+# 		node.set_process(true)
+# 	for node in nodes_to_deactivate:
+# 		node.set_process(false)
+# 	for node in nodes_to_show:
+# 		node.show()
+# 	for node in nodes_to_hide:
+# 		node.hide()
