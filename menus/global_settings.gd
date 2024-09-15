c42e599d8c78fa0c8e91d7a8eb11850a45a971cc
Henri Pätsi <patsihenri@gmail.com>
2024-09-15 09:30:11 +0300

    gameplat sequence fixes

--- menus/global_settings.gd
+++ menus/global_settings.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Settings
 var mouse_sensitivity_modifier: float = 1
-var volume_db: float = -20
+var volume_db: float = -12
 var crosshair_opacity: float = 1
 var crosshair_size: float = 5
 var text_size: int = 32
