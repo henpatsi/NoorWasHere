34896a8d5650b184581b55594599ae4fe0ef4cc7
Henri Pätsi <patsihenri@gmail.com>
2024-09-15 12:50:31 +0300

    deleted missing, compressed

--- levels/scripts/subtitled_audio_clip.gd
+++
@@ -1,9 +0,0 @@
-class_name SubtitleAudioClip
-extends Resource
-
-@export var audio_clip: AudioStream
-@export var subtitle: String
-
-func _init(p_audio_clip = null, p_subtitle = null):
-	audio_clip = p_audio_clip
-	subtitle = p_subtitle
