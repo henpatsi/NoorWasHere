749af86b94a39834f0dae296d696a2e753543db1
Henri Pätsi <patsihenri@gmail.com>
2024-10-14 09:44:23 +0300

    started working on footstep refactor

--- player/footsteps/footsteps.gd
+++ player/footsteps/footsteps.gd
@@ -1,11 +1,25 @@
 extends AudioStreamPlayer3D
 
-var rng = RandomNumberGenerator.new()
+const FOOTSTEP_GROUPS = ["FootstepDefault", "FootstepWood", "FootstepGrass"]
 
 @export var footstep_duration: float = 0.5
-@export var footstep_sound_effects: Array[AudioStream]
+## Played if ground type is not set or for which separate sound effects do not exist
+@export var default_footstep_sound_effects: Array[AudioStream]
+## Played if ground type is wood
+@export var wood_footstep_sound_effects: Array[AudioStream]
+## Played if ground type is grass
+@export var grass_footstep_sound_effects: Array[AudioStream]
 
+var footstep_sound_effects: Dictionary = {
+											FOOTSTEP_GROUPS[0]: default_footstep_sound_effects,
+											FOOTSTEP_GROUPS[1]: wood_footstep_sound_effects,
+											FOOTSTEP_GROUPS[2]: grass_footstep_sound_effects
+										}
+
+
+var rng = RandomNumberGenerator.new()
 var footstep_timer: float = 0.0
+
 
 func movement_process(delta: float) -> void:
 	footstep_timer += delta;
@@ -17,5 +31,6 @@ func movement_process(delta: float) -> void:
 
 
 func play_random_footstep() -> void:
-	stream = footstep_sound_effects[rng.randi_range(0, footstep_sound_effects.size() - 1)]
+
+	stream = default_footstep_sound_effects[rng.randi_range(0, default_footstep_sound_effects.size() - 1)]
 	play()
