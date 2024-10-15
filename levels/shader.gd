b0b83f0d53b3a23086f2bab7a569a910f25ba4b9
themrpatsi <53088049+themrpatsi@users.noreply.github.com>
2024-10-15 15:14:45 +0300

    tested env and stuff

--- levels/shader.gd
+++ levels/shader.gd
@@ -3,12 +3,15 @@ extends ColorRect
 @onready var world_environment: WorldEnvironment = $"../WorldEnvironment"
 var ENV_PAST = preload("res://assets/WorldEnvironments/Env_Past.tres")
 var ENV_PRESENT = preload("res://assets/WorldEnvironments/Env_Present.tres")
+@onready var shadow_catcher: MeshInstance3D = $"../ShadowCatcher"
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shader"):
 		if world_environment.environment == ENV_PRESENT:
 			world_environment.environment = ENV_PAST
-			visible = false
+			visible = true
+			shadow_catcher.visible = true
 		else:
 			world_environment.environment = ENV_PRESENT
-			visible = true
+			visible = false
+			shadow_catcher.visible = false
