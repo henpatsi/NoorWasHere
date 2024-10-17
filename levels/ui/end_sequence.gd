8ba8d0ecbbb59a37bf039bc71668b3639ea4b56f
henpatsi <patsihenri@gmail.com>
2024-10-17 13:56:05 +0300

    added end sequence, still in wrong order

--- levels/ui/end_sequence.gd
+++ levels/ui/end_sequence.gd
@@ -1,9 +1,10 @@
 extends Control
 
 @export var player: CharacterBody3D
-@export var picture_screenshots: Array[TextureRect]
+
+@export var texture_rect: TextureRect
+@export var picture_screenshot_paths: Array[String]
 @export var show_times: Array[float]
-@export var player_dialogues_ASP: AudioStreamPlayer3D
 
 @export var level: Node
 @export var end_menu_path: String = "res://menus/end_menu.tscn"
@@ -17,18 +18,24 @@ func play_end_sequence() -> void:
 	picture_handler.set_input_state(false)
 	picture_handler.process_mode = Node.PROCESS_MODE_DISABLED
 	#player.process_mode = Node.PROCESS_MODE_DISABLED
+	
+	texture_rect.show()
 
 	var i: int = 0
-	for screenshot in picture_screenshots:
-		screenshot.get_parent().remove_child(screenshot)
-		add_child(screenshot)
-		screenshot.position = Vector2(0, 0)
-		screenshot.size = Vector2(1280, 720)
-		screenshot.show()
+	for path in picture_screenshot_paths:
+		var image = load(path)
+		texture_rect.texture = image
+		#screenshot.get_parent().remove_child(screenshot)
+		#add_child(screenshot)
+		#screenshot.position = Vector2(0, 0)
+		#screenshot.size = Vector2(1280, 720)
+		#screenshot.show()
 		await get_tree().create_timer(show_times[i]).timeout
 		i += 1
-		screenshot.hide()
-	
+		#screenshot.hide()
+
 	#level.load_scene(end_menu_path)
 	color_rect.show()
 	label.show()
+	
+	texture_rect.hide()
